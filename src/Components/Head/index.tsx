91d1f9b384d51b658f9c721aa16225182884c0fb
Ezequiassantos11 <ezequiasmg16@gmail.com>
2022-05-10 13:03:58 -0300

    mudando o head

--- src/Components/Head/index.tsx
+++ src/Components/Head/index.tsx
@@ -9,7 +9,7 @@ export default function Heads() {
         href="https://fonts.googleapis.com/css2?family=Poppins:wght@700&family=Roboto:wght@400;700&display=swap"
         rel="stylesheet"
       />
-      <title>Ezequias Blog's
+      <title>Ezequias Santos
       </title>
     </Head>
   );
